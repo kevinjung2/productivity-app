276671c330f541a9c6440357e11a15e0ed0484de
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 16:31:03 -0400

    adds table to create amounts

--- db/migrate/20210401194617_create_amounts.rb
+++ db/migrate/20210401194617_create_amounts.rb
@@ -1,7 +1,8 @@
 class CreateAmounts < ActiveRecord::Migration[6.1]
   def change
     create_table :amounts do |t|
-
+      t.integer :item_id
+      t.integer :shopping_list_id
       t.timestamps
     end
   end

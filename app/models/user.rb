962e3e36d6d2c3f0f45af00a4088c8d8cf60b86f
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 16:54:24 -0400

    adds bcrypt and secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :user_events
   has_many :events, through: :user_events
   has_many :hosted_events, foreign_key: :host_id, class_name: "Event"

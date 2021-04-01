30cf55e621166b6849fda412e437826e72c972ff
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 16:32:15 -0400

    adds table to create user_events

--- db/migrate/20210401194658_create_user_events.rb
+++ db/migrate/20210401194658_create_user_events.rb
@@ -1,7 +1,8 @@
 class CreateUserEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :user_events do |t|
-
+      t.integer :user_id
+      t.integer :event_id
       t.timestamps
     end
   end

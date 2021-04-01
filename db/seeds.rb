7752d8addef418e42f16a43a01e8484f2d0ee37e
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 19:05:12 -0400

    seeds the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = []
+x = 1
+10.times do
+  user << "User#{x}"
+  x += 1
+end
+
+user.each do |u|
+  User.create(name: u, password: "password")
+end
+
+event = Event.create(name: "My Event")
+event.host = User.first
+event.users << User.last
+event.save

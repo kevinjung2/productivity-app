5e1941d48117dfb6509bfcb0836ce50f6c3ad41a
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 23:53:02 -0400

    adds redirect helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     def find_user
       User.find_by(id: params[:id])
     end
+
+    def redirect_if_not_logged_in
+      redirect_to '/login' if !session[:user_id]
+    end
+
+    def redirect_if_user_doesnt_match(user_id)
+      redirect_to '/profile' if current_user != User.find_by(id: user_id)
+    end
 end

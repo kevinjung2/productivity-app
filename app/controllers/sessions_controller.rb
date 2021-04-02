fd18312c24dfd6e447d9a5859bdfd169baa59dd4
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 23:01:49 -0400

    adds login and logout routes and respective session controller actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+      else
+        redirect_to '/login', alert: "Incorrect Password"
+      end
+    else
+      redirect_to '/login', alert: "Name not found"
+    end
+
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :show]
   get '/signup', to: 'users#new'
   get '/profile', to: 'users#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

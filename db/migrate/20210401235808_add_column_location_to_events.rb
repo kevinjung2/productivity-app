e3fb1676ca5c344f9cecfad2e9eea51a4b72e6b0
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-01 20:05:16 -0400

    adds location column to events

---
+++ db/migrate/20210401235808_add_column_location_to_events.rb
@@ -0,0 +1,5 @@
+class AddColumnLocationToEvents < ActiveRecord::Migration[6.1]
+  def change
+    add_column :events, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_230744) do
+ActiveRecord::Schema.define(version: 2021_04_01_235808) do
 
   create_table "amounts", force: :cascade do |t|
     t.integer "item_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_230744) do
     t.datetime "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
   end
 
   create_table "items", force: :cascade do |t|

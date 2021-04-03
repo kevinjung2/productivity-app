49f06cd1e9e17357227a0bd952320ca7dc0188a2
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-03 08:25:50 -0400

    adds RUD skeleton to shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,2 +1,27 @@
 class ShoppingListsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def shopping_list_params
+
+    end
 end

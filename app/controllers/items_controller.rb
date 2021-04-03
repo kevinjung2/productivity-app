4213e6f6ed58dc100dcee26d2fdf532c43a5321b
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-03 07:37:19 -0400

    adds basic CRUD skeleton for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,35 @@
 class ItemsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def item_params
+
+    end
 end

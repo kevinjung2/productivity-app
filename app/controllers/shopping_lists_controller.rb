28d326ff1d14c263e27d6bce204ff5435a64c88d
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-03 08:49:36 -0400

    adds index view for shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,6 +1,9 @@
 class ShoppingListsController < ApplicationController
   def index
+    @user = User.find_by(id: params[:user_id])
+    redirect_to '/profile', alert: "you can only see your shopping lists" if @user != current_user
 
+    @shopping_lists = @user.shopping_lists 
   end
 
   def show

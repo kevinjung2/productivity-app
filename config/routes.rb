29086481a1d5a079410a4eaf88f6bc5481e6b86a
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-04 16:12:06 -0400

    adds navbar styling

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

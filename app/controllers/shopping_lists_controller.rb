25eebf7a108eb1b28a1193b2452a7837863d2e9c
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-03 23:05:24 -0500

    adds show view for shopping lists

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,11 +3,21 @@ class ShoppingListsController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     redirect_to '/profile', alert: "you can only see your shopping lists" if @user != current_user
 
-    @shopping_lists = @user.shopping_lists 
+    @shopping_lists = @user.shopping_lists
   end
 
   def show
-
+    @shopping_list = ShoppingList.find_by(id: params[:id])
+    @categories = Category.all
+    @items = {}
+    #this finds all items in the shopping list and returns them to the variable @items in a hash structured as {"category name" -> <itemobj>}
+    @categories.each do |category|
+      item_array = []
+      category.amounts.each do |amount|
+        item_array << amount if @shopping_list.amounts.include?(amount)
+      end
+      @items["#{category.name}"] = item_array
+    end
   end
 
   def edit

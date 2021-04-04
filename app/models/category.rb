790aa8a05d9edbe61c83d075133cf3e2d6f0f310
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-04 13:06:25 -0500

    adds method to build all categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   has_many :items
   has_many :amounts, through: :items
+
+  CATEGORY_NAMES = ["Undefined", "Food", "Decorations", "Serviceware", "Games", "Furniture", "Clothing"]
+
+  def self.create_categories
+    CATEGORY_NAMES.each do |name|
+      Category.find_or_create_by(name: name)
+    end
+  end
 end

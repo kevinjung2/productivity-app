94a41f2d43b08125c607837f0aef8299988c7c1b
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-04 19:12:38 -0400

    adds items to navbar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    redirect_if_not_logged_in
     @items = {}
     @categories = Category.all
     @categories.each do |category|
@@ -8,31 +9,37 @@ class ItemsController < ApplicationController
   end
 
   def show
+    redirect_if_not_logged_in
     @item = Item.find_by(id: params[:id])
   end
 
   def new
+    redirect_if_not_logged_in
     @item = Item.new
     @categories = Category.all
   end
 
   def create
+    redirect_if_not_logged_in
     item = Item.create(item_params)
     redirect_to item, alert: "Successfully created item."
   end
 
   def edit
+    redirect_if_not_logged_in
     @item = Item.find_by(id: params[:id])
     @categories = Category.all
   end
 
   def update
+    redirect_if_not_logged_in
     item = Item.find_by(id: params[:id])
     item.update(item_params)
     redirect_to item, alert: "Successfully updated item."
   end
 
   def destroy
+    redirect_if_not_logged_in
     item = Item.find_by(id: params[:id])
     name = item.name
     item.destroy
